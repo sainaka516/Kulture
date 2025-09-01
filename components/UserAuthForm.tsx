f8b15210097b72ea0814b57a6c0e7fad19d04e28
SaiNaka-516 <scnaka2@illinois.edu>
2025-09-01 17:15:28 -0500

    Trynna get my boy vercel up and running

--- components/UserAuthForm.tsx
+++ components/UserAuthForm.tsx
@@ -79,6 +79,7 @@ export default function UserAuthForm({
         })
 
         const result = await response.json()
+        console.log('result', result)
         console.log('[AUTH] Signup response:', { status: response.status, ...result })
 
         if (!response.ok) {
