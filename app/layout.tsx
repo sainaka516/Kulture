24bb08eb4cd9d7ce7d8756a83c08c87d4f3e441b
SaiNaka-516 <scnaka2@illinois.edu>
2025-09-02 00:06:05 -0500

    Add debugging to identify button interaction issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  console.log('[LAYOUT] Rendering root layout')
+  
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}>
@@ -52,7 +54,6 @@ export default function RootLayout({
               </div>
             </div>
             <Toaster />
-            <DevSessionHelper />
           </AuthProvider>
         </ThemeProvider>
       </body>
